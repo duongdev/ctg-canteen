24597d7c258fe1a490f16790dbfe2624c72a289e
Dương Đỗ <dustin.do95@gmail.com>
2019-06-18 00:29:49 +0700

    feat: add xlsx reader, parse to json

--- packages/web/src/containers/Users/components/UserImport/UserImport.tsx
+++ packages/web/src/containers/Users/components/UserImport/UserImport.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
 
+import { Box, Container, Grid } from '@material-ui/core'
+import * as XLSX from 'xlsx'
+import UserImportDropzone from './components/UserImportDropzone'
+
 type UserImportProps = {}
 
 const UserImport: React.FC<UserImportProps> = () => {
-  return <>UserImport</>
+  const [file, setFile] = React.useState<File | null>(null)
+
+  React.useEffect(() => {
+    if (!file) return
+
+    const reader = new FileReader()
+    const rABS = !!reader.readAsBinaryString
+    reader.onload = (e: any) => {
+      /* Parse data */
+      const bstr = e.target.result
+      const wb = XLSX.read(bstr, { type: rABS ? 'binary' : 'array' })
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0]
+      const ws = wb.Sheets[wsname]
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_json(ws, { header: 2 })
+      /* Update state */
+      console.log(data)
+    }
+    if (rABS) reader.readAsBinaryString(file)
+    else reader.readAsArrayBuffer(file)
+  }, [file])
+
+  return (
+    <Box mt={8} mb={8}>
+      <Container maxWidth="md">
+        <Grid container spacing={2} direction="column">
+          <Grid item>
+            <UserImportDropzone file={file} setFile={setFile} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  )
 }
 
 export default UserImport
