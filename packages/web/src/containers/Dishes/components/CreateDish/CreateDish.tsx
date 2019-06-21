a97b2967e7733ee21707314b1f5264bdd4aeba8c
Dương Đỗ <dustin.do95@gmail.com>
2019-06-21 20:04:08 +0700

    feat(web): [CreateDish] add dish form

--- packages/web/src/containers/Dishes/components/CreateDish/CreateDish.tsx
+++ packages/web/src/containers/Dishes/components/CreateDish/CreateDish.tsx
@@ -1,14 +1,62 @@
 import React from 'react'
 
+import { Grid, TextField } from '@material-ui/core'
 import ContentContainer from 'components/ContentContainer'
 import Section from 'components/Section'
+import { Field, FieldProps, Form, Formik } from 'formik'
+
+const initialValues = {
+  name: '',
+  description: '',
+}
+
+type Values = typeof initialValues
 
 type CreateDishProps = {}
 
 const CreateDish: React.FC<CreateDishProps> = () => {
   return (
     <ContentContainer maxWidth="sm">
-      <Section title="Thêm món ăn mới">Giao diện thêm món ăn mới nè</Section>
+      <Section title="Thêm món ăn mới">
+        <Formik initialValues={initialValues} onSubmit={console.log}>
+          {(form) => (
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Field
+                    name="name"
+                    render={({ field }: FieldProps<Values>) => (
+                      <TextField
+                        variant="outlined"
+                        autoFocus
+                        fullWidth
+                        label="Tên món ăn"
+                        {...field}
+                      />
+                    )}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    name="description"
+                    render={({ field }: FieldProps<Values>) => (
+                      <TextField
+                        variant="outlined"
+                        fullWidth
+                        label="Mô tả món ăn"
+                        multiline
+                        rows={2}
+                        rowsMax={4}
+                        {...field}
+                      />
+                    )}
+                  />
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </Section>
     </ContentContainer>
   )
 }
