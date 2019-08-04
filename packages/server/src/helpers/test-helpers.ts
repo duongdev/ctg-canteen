c8889c06a7be917fd79855271763f99c13181226
Nguyen Thanh <nguyenthienthanh.kiyoshi@gmail.com>
2019-08-04 17:24:02 +0700

    feat(server): add mockingoose

--- packages/server/src/helpers/test-helpers.ts
+++ packages/server/src/helpers/test-helpers.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server'
 import { ApolloServerExpressConfig } from 'apollo-server-express'
 import { createTestClient as createTestApolloClient } from 'apollo-server-testing'
 import { merge } from 'lodash'
+import mockingoose from 'mockingoose'
 import mongoose from 'mongoose'
 import resolvers from 'resolvers'
 import schemas from 'schemas'
@@ -22,3 +23,5 @@ export const createTestClient = (config: ApolloServerExpressConfig = {}) => {
 }
 
 export const getObjectId = mongoose.Types.ObjectId
+
+export const mockingooseResetAll = () => mockingoose.resetAll()
