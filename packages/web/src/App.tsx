e04e63485401f2acf6cc43945cc4bad99fa0deed
Dương Đỗ <dustin.do95@gmail.com>
2019-08-04 17:25:34 +0700

    feat(web): add material-ui and react-router

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { CssBaseline } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/styles'
+import theme from 'theme'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      Hello
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
