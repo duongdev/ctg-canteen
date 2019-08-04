a6c35cbe9c76901b293bbbafc937e293676c7a59
Dương Đỗ <dustin.do95@gmail.com>
2019-08-04 17:10:00 +0700

    feat(web): re-initialize create-react-app

--- packages/web/src/components/Helmet/Helmet.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import ReactHelmet, { HelmetProps } from 'react-helmet'
-
-const Helmet: React.FC<HelmetProps> = (props) => (
-  <ReactHelmet defer {...props} />
-)
-
-export default Helmet
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,23 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import { CssBaseline } from '@material-ui/core'
-import { ThemeProvider } from '@material-ui/styles'
-import theme from 'theme'
-import Router from './Router'
-import * as serviceWorker from './serviceWorker'
-
-import './index.css'
-
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <Router />
-  </ThemeProvider>,
-  document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
