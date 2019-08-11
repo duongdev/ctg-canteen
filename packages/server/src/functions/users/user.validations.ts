f18562b93cb218aa1581a38bc9c75e05683b22aa
Nguyen Thanh <nguyenthienthanh.kiyoshi@gmail.com>
2019-08-11 23:15:20 +0700

    feat(server): add baseUserValidation

--- packages/server/src/functions/users/user.validations.ts
+++ packages/server/src/functions/users/user.validations.ts
@@ -1,50 +1,50 @@
 import { USER_CLASSES, USER_GROUPS, USER_ROLES, USER_SEX } from 'models/User'
 import * as yup from 'yup'
 
-export const createUserValidation = yup
-  .object()
-  .shape({
-    studentId: yup.string().trim(),
-    username: yup.string().trim(),
-    name: yup
-      .string()
-      .trim()
-      .required(),
-    checkerId: yup.string().trim(),
-    birthdate: yup.date().required(),
-    hometown: yup
-      .string()
-      .trim()
-      .required(),
-    sex: yup
-      .string()
-      .trim()
-      .oneOf(USER_SEX)
-      .required(),
-    class: yup
-      .string()
-      .trim()
-      .oneOf(USER_CLASSES)
-      .required(),
-    schoolYear: yup
-      .number()
-      .moreThan(1900)
-      .required(),
-    group: yup
-      .string()
-      .oneOf(USER_GROUPS)
-      .required(),
-    boardingRoom: yup
-      .string()
-      .trim()
-      .required(),
-    password: yup
-      .string()
-      .trim()
-      .ensure()
-      .required(),
-    roles: yup.array().of(yup.string().oneOf(USER_ROLES)),
-  })
+const baseUserValidation = yup.object().shape({
+  studentId: yup.string().trim(),
+  username: yup.string().trim(),
+  name: yup
+    .string()
+    .trim()
+    .required(),
+  checkerId: yup.string().trim(),
+  birthdate: yup.date().required(),
+  hometown: yup
+    .string()
+    .trim()
+    .required(),
+  sex: yup
+    .string()
+    .trim()
+    .oneOf(USER_SEX)
+    .required(),
+  class: yup
+    .string()
+    .trim()
+    .oneOf(USER_CLASSES)
+    .required(),
+  schoolYear: yup
+    .number()
+    .moreThan(1900)
+    .required(),
+  group: yup
+    .string()
+    .oneOf(USER_GROUPS)
+    .required(),
+  boardingRoom: yup
+    .string()
+    .trim()
+    .required(),
+  password: yup
+    .string()
+    .trim()
+    .ensure()
+    .required(),
+  roles: yup.array().of(yup.string().oneOf(USER_ROLES)),
+})
+
+export const createUserValidation = baseUserValidation
   .test(
     'at-least-one-studentId-or-username',
     'you must provide at least one of studentId or username',
@@ -59,41 +59,9 @@ export const createStudentsValidation = yup
   .of(
     yup
       .object()
+      .concat(baseUserValidation)
       .shape({
         studentId: yup
-          .string()
-          .trim()
-          .required(),
-        username: yup.string().trim(),
-        name: yup
-          .string()
-          .trim()
-          .required(),
-        checkerId: yup.string().trim(),
-        birthdate: yup.date().required(),
-        hometown: yup
-          .string()
-          .trim()
-          .required(),
-        sex: yup
-          .string()
-          .trim()
-          .oneOf(USER_SEX)
-          .required(),
-        class: yup
-          .string()
-          .trim()
-          .oneOf(USER_CLASSES)
-          .required(),
-        schoolYear: yup
-          .number()
-          .moreThan(1900)
-          .required(),
-        group: yup
-          .string()
-          .oneOf(USER_GROUPS)
-          .required(),
-        boardingRoom: yup
           .string()
           .trim()
           .required(),
