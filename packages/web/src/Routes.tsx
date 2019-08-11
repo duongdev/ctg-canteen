efa4e7ba98d718ebbec17e4bacd9e3562d979610
Dương Đỗ <dustin.do95@gmail.com>
2019-08-11 16:37:12 +0700

    feat(web): add redirect to sign-in page from homepage

--- packages/web/src/Routes.tsx
+++ packages/web/src/Routes.tsx
@@ -2,13 +2,16 @@ import React, { FC } from 'react'
 
 import Dashboard from 'containers/Dashboard'
 import SignIn from 'containers/SignIn'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 
 const Routes: FC = () => {
   return (
     <BrowserRouter>
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/sign-in" component={SignIn} />
+      <Switch>
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/sign-in" component={SignIn} />
+        <Redirect to="/sign-in" />
+      </Switch>
     </BrowserRouter>
   )
 }
