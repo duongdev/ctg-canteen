1d3eb5ab3347227dd2b89f7ef4c92ea545f1a1df
Nguyen Thanh <nguyenthienthanh.kiyoshi@gmail.com>
2019-08-18 17:57:40 +0700

    feat(server): [resolver] add hasRole hook

--- packages/server/src/helpers/resolvers.ts
+++ packages/server/src/helpers/resolvers.ts
@@ -1,5 +1,5 @@
 import { GraphQLResolveInfo } from 'graphql'
-import { IUser } from 'models/User'
+import { IUser, UserRole } from 'models/User'
 import yup from 'yup'
 
 export type ResolverFn<
@@ -20,6 +20,7 @@ export type ResolverOptions<ReturnedValue, Args, Context, RootValue> = {
   paramsValidationSchema?: yup.MixedSchema<Args>
   /** Do you want to use some hooks? */
   use?: {
+    hasRole?: UserRole
     /** Throws "unauthorized" error if ctx.user is empty. */
     isUser?: boolean
   }
@@ -36,6 +37,24 @@ const isUser = <RootValue, Args>(
   _info: GraphQLResolveInfo,
 ) => {
   if (!ctx.user) throw new Error('unauthorized')
+}
+
+const hasRole = <RootValue, Args>(
+  parent: RootValue,
+  args: Args,
+  ctx: BaseContext,
+  info: GraphQLResolveInfo,
+) => (role: UserRole) => {
+  isUser(parent, args, ctx, info)
+  if (
+    !ctx.user.roles ||
+    ctx.user.roles.includes('deactivated') ||
+    !ctx.user.roles.includes(role)
+  ) {
+    throw new Error('unauthorized')
+  }
+
+  return
 }
 
 export const createResolver = <
@@ -55,6 +74,10 @@ export const createResolver = <
 
   if (use) {
     if (use.isUser) isUser(parent, args, context, info)
+
+    if (use.hasRole) {
+      hasRole(parent, args, context, info)(use.hasRole)
+    }
   }
 
   const validatedArgs = paramsValidationSchema
