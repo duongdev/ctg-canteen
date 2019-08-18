c542baacc86b00679f6126573aa1e525d5dffa69
Dương Đỗ <dustin.do95@gmail.com>
2019-08-18 20:07:44 +0700

    fix(web): [netlify] add missing REACT_APP_SERVER_URI on master

--- scripts/netlify-deploy.bash
+++ scripts/netlify-deploy.bash
@@ -5,6 +5,8 @@ echo CONTEXT=$CONTEXT REVIEW_ID=$REVIEW_ID
 if [ $CONTEXT == "deploy-preview" ]; then
   echo "Applying PR SERVER_URI"
   export REACT_APP_SERVER_URI=https://ctg-canteen-pr-$REVIEW_ID.herokuapp.com/graphql
+else
+  export REACT_APP_SERVER_URI=https://ctg-canteen.herokuapp.com/graphql
 fi
 
 echo REACT_APP_SERVER_URI=$REACT_APP_SERVER_URI
